777bd535c584d4d715883bf491b6e713c23e69b5
Peter Burkholder <peterb@getchef.com>
2014-10-06 13:05:05 -0400

    Fails but with two vars

--- partial_templates/recipes/default.rb
+++ partial_templates/recipes/default.rb
@@ -1,8 +1,11 @@
 local_stuff='local variable content'
-apps=['appserver']
+my_apps=['appserver']
 
 template '/etc/foo' do
   source     'foo.erb'
-  variables  local_data: local_stuff
-  #, apps: apps
+#  variables  local_data: local_stuff
+  variables  ({
+    local_data: local_stuff,
+    afsdlj: "asdlfj"
+  })
 end
