12b1abf4f4cf8d63832c822ebcd1ce42ffa1395b
Peter Burkholder <peterb@getchef.com>
2014-10-06 11:33:36 -0400

    Allowing for future json path specification

--- cookbooks/jsondemo/libraries/helper.rb
+++ cookbooks/jsondemo/libraries/helper.rb
@@ -4,7 +4,7 @@ def json_path(json_file='data.json')
   File.join(File.dirname(__FILE__), json_file)
 end
 
-def fetch_data(duty='appserver')
-  duties=JSON.parse(File.read(json_path))
+def fetch_data(duty='appserver', json='data.json')
+  duties=JSON.parse(File.read(json_path(json)))
   return duties[duty]
 end
