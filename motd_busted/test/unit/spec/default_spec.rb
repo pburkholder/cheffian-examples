df0ffe5e8f5a6363828e21250fca822bd428b393
Peter Burkholder <peterb@getchef.com>
2014-11-06 12:54:20 -0500

    Simplify spec

--- motd_busted/test/unit/spec/default_spec.rb
+++ motd_busted/test/unit/spec/default_spec.rb
@@ -4,10 +4,6 @@ require_relative 'spec_helper'
 describe 'motd::default' do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
-  it 'should do converge' do
-    expect(chef_run).to be
-  end
-
   it 'should create MOTD' do
     expect(chef_run).to render_file('/etc/motd')
       .with_content('Awesome Appliance')
