6220df1e935aad8e111cebfb22e46984799f146e
Peter Burkholder <peterb@getchef.com>
2014-11-07 12:01:24 -0500

    Sensible README

--- motd_busted/metadata.rb
+++ motd_busted/metadata.rb
@@ -1,5 +1,7 @@
 # Encoding: utf-8
+# Correct next line to disable RuboCop warning
+# rrrrubocop:disable SingleSpaceBeforeFirstArg
 name                'motd'
 license             'Apache 2.0'
-description         'Example JSON data structure'
+description         'Example of testing'
 version             '0.1.0'
