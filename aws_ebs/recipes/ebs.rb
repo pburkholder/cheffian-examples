4f6c416b9f52034297c15cab0df3bc4ef72eec36
Peter Burkholder <peterb@getchef.com>
2014-12-31 11:56:21 -0500

    WIP on aws-ebs example

--- aws_ebs/recipes/ebs.rb
+++ aws_ebs/recipes/ebs.rb
@@ -4,4 +4,16 @@
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 
-log "log ebs recipe"
+include_recipe 'aws'
+
+aws_ebs_volume "db_ebs_volume" do
+  aws_access_key node['aws_ebs']['aws_access_key_id']
+  aws_secret_access_key node['aws_ebs']['aws_secret_access_key']
+  size 50
+  device "/dev/sdi"
+  action [ :create, :attach ]
+end
+
+mount '/mnt/db' do
+  device 'dev/sdi'
+end
