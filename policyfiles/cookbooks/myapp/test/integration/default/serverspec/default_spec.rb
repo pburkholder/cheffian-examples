05bd20a5f7787ee02f949ba5a2edb41cd51ebb9c
Peter Burkholder <pburkholder@chef.io>
2015-04-15 12:22:24 -0400

    Adding simple serverspec

--- policyfiles/cookbooks/myapp/test/integration/default/serverspec/default_spec.rb
+++ policyfiles/cookbooks/myapp/test/integration/default/serverspec/default_spec.rb
@@ -5,8 +5,8 @@ describe 'myapp::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
   
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe command("curl -L localhost | grep -a 'Build:'") do
+        its(:exit_status) { should eq 0 }
   end
 
 end
