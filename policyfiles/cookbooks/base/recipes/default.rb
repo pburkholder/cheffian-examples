0211fbb176739b700537a7bbd314a5f8c263dc69
Peter Burkholder <pburkholder@chef.io>
2015-04-15 12:25:04 -0400

    Fix order of apt-get updated

--- policyfiles/cookbooks/base/recipes/default.rb
+++ policyfiles/cookbooks/base/recipes/default.rb
@@ -4,12 +4,11 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+include_recipe 'apt::default' # ensure apt-update runs
+
 %w(curl lsof).each do |p|
   package p
 end
-
-
-include_recipe 'apt::default' # ensure apt-update runs
 
 httpd_service 'default' do
   action [:create, :start]
