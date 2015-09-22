d99c2b20ddbbff5e06064ebb4c59230ecb07008a
Peter Burkholder <pburkholder@pobox.com>
2015-09-22 16:59:37 -0400

    Works with simplest edb

--- chefspec_encrypted_db/recipes/default.rb
+++ chefspec_encrypted_db/recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+#db_pwd = Chef::EncryptedDataBagItem.load("pw", "mssql", Chef::EncryptedDataBagItem.load_secret("#{data_bag_item("keys", "admin")['keyfile']}"))["#{node.environment}"]["db_password"]
+#db_client_pwd = Chef::EncryptedDataBagItem.load("pw", "mssql", Chef::EncryptedDataBagItem.load_secret("#{data_bag_item("keys", "admin")['keyfile']}"))["#{node.environment}"]["db_client_password"]
+
+cleartext = 'cleartext'
+simple_edb = Chef::EncryptedDataBagItem.load("simple", "edb", "secret")
+
+file '/etc/cleartext' do
+  content "Welcome to secret: #{cleartext}"
+end
+
+file '/etc/simple_edb' do
+  content "Welcome to secret: #{simple_edb}"
+end
--- chefspec_encrypted_db/spec/unit/recipes/default_spec.rb
+++ chefspec_encrypted_db/spec/unit/recipes/default_spec.rb
@@ -9,12 +9,27 @@ require 'spec_helper'
 describe 'chefspec_encrypted_db::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::SoloRunner.new
       runner.converge(described_recipe)
+    end
+
+    before do
+      allow(Chef::EncryptedDataBagItem).to receive(:load)
+        .with('simple', 'edb', 'secret').and_return('simple_edb')
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'makes /etc/cleartext' do
+      expect(chef_run).to render_file('/etc/cleartext').
+        with_content('Welcome to secret: cleartext')
+    end
+
+    it 'makes /etc/simple_edb' do
+      expect(chef_run).to render_file('/etc/simple_edb').
+        with_content('Welcome to secret: simple_edb')
     end
   end
 end
